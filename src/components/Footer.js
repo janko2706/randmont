a9e74fa38b4fdf9a341126a3f652a522fc386d97
Jan Bogovic <1087894@ucn.dk>
2022-08-02 18:20:26 +0200

    minor fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -21,7 +21,7 @@ function Footer() {
                     <div class="footer-menu">
                         <ul class="f-menu">
                             <li><a href="/">Home</a></li>
-                            <li><a href="/aboutus">About</a></li>
+                            <li><a href="/about">About</a></li>
                             <li><a href="/contact">Contact</a></li>
                             <li><a href="/services">Services</a></li>
                         </ul>
